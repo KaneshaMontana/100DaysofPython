99d58f4721a37fc78ae911e3e4c7a1ddd25a70cc
Kanesha <93129317+KaneshaMontana@users.noreply.github.com>
2023-07-05 16:08:05 +0100

    Create Calculator.py

---
+++ Day10/Calculator.py
@@ -0,0 +1,48 @@
+import logo from art.py
+
+def add(n1, n2):
+  return n1 + n2
+
+def subtract(n1, n2):
+  return n1 - n2
+
+def multiply(n1, n2):
+  return n1 * n2
+
+def divide(n1, n2):
+  return n1 / n2
+
+operations = {
+  "+": add,
+  "-": subtract,
+  "*": multiply,
+  "/": divide
+}
+
+# Pick the first number
+def calculator():
+  print(logo)
+  num1 = float(input("What's the first number?: "))
+
+  for symbol in operations:
+    print(symbol)
+  should_continue = True
+
+while should_continue:
+  operation_symbol = input("Pick an operation: ") 
+  next_num = float(input("What's the next number?: "))
+  calculation_function = operations[operation_symbol]
+  answer = calculation_function(num1, next_num)
+  
+  print(f"{num1} {operation_symbol} {next_num} = {answer}")
+  
+  continue_calculation = input(f"Type 'y' to continue calculating with {answer}, or type 'n' for a new calculation.: ")
+  if continue_calculation == 'y':
+    should_continue = True
+    num1 = answer
+  elif continue_calculation == 'n':
+    should_continue = False
+    print(f"Your final answer is {answer} ")
+    calculator()
+
+calculator()
