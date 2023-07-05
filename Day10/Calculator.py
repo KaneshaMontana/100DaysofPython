e5afe188d0ea9e38d65ef7d2c3810aa141daeecb
Kanesha <93129317+KaneshaMontana@users.noreply.github.com>
2023-07-05 16:16:24 +0100

    Update Calculator.py

--- Day10/Calculator.py
+++ Day10/Calculator.py
@@ -20,7 +20,7 @@ operations = {
 }
 
 # Pick the first number
-def calculator():
+def calculator(): 
   print(logo)
   num1 = float(input("What's the first number?: "))
 
@@ -28,21 +28,21 @@ def calculator():
     print(symbol)
   should_continue = True
 
-while should_continue:
-  operation_symbol = input("Pick an operation: ") 
-  next_num = float(input("What's the next number?: "))
-  calculation_function = operations[operation_symbol]
-  answer = calculation_function(num1, next_num)
-  
-  print(f"{num1} {operation_symbol} {next_num} = {answer}")
-  
-  continue_calculation = input(f"Type 'y' to continue calculating with {answer}, or type 'n' for a new calculation.: ")
-  if continue_calculation == 'y':
-    should_continue = True
-    num1 = answer
-  elif continue_calculation == 'n':
-    should_continue = False
-    print(f"Your final answer is {answer} ")
-    calculator()
+  while should_continue:
+    operation_symbol = input("Pick an operation: ") 
+    next_num = float(input("What's the next number?: "))
+    calculation_function = operations[operation_symbol]
+    answer = calculation_function(num1, next_num)
+    
+    print(f"{num1} {operation_symbol} {next_num} = {answer}")
+    
+    continue_calculation = input(f"Type 'y' to continue calculating with {answer}, or type 'n' for a new calculation.: ")
+    if continue_calculation == 'y':
+      should_continue = True
+      num1 = answer
+    elif continue_calculation == 'n':
+      should_continue = False
+      print(f"Your final answer is {answer} ")
+      calculator()
 
 calculator()
